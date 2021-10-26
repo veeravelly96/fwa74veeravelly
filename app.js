e7906d0ea247428a41145a8ad9854894e6ab5f1d
sai krishna veeravelly <S544724@NWMISSOURI.EDU>
2021-10-26 18:37:30 -0500

    bonus updated

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mydata = require('./routes/mydata');
+var compute = require('./routes/compute');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mydata', mydata);
+app.use('/compute',compute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
