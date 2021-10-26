57d889badaca4d08d2ceb5e2b50763a718714c44
sai krishna veeravelly <S544724@NWMISSOURI.EDU>
2021-10-26 18:20:10 -0500

    updated

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var usersRouter = require('./routes/mydata');
+var mydata = require('./routes/mydata');
 
 var app = express();
 
